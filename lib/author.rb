04f52510d95221f19ca4b70b3aec662c801d3f79
Elijah Kirschen <ekirschen584@gmail.com>
2020-06-12 01:22:32 +0000

    Automatically backed up by Learn

--- lib/author.rb
+++ lib/author.rb
@@ -1,34 +1,30 @@
-require 'pry'
-
 class Author
+  
+  attr_accessor :name
 
-  attr_accessor :name, :posts
-
-  @@post_count = 0
+@@all =[]
   
   def initialize(name)
     @name = name
-    @posts = []
+    @post = []
   end
-
-  def posts 
   
-  end
-
   def add_post(post)
-    @posts << post
     post.author = self
-    @@post_count += 1
+    @post << post
+  end
+  
+  
+ def posts
+   Post.all.select {|post| post.author == self}
   end
 
   def add_post_by_title(title)
     post = Post.new(title)
-    @posts << post
-    post.author = self
-    @@post_count += 1
+    add_post(post)
   end
-
+  
   def self.post_count
-    @@post_count
+    Post.all.count 
   end
 end
