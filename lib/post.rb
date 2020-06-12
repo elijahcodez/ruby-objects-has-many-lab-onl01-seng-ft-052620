e0fb5b7ac3e78c2b1730f5fe8555e9ba41b01b14
Elijah Kirschen <ekirschen584@gmail.com>
2020-06-12 01:22:59 +0000

    Automatically backed up by Learn

--- lib/post.rb
+++ lib/post.rb
@@ -1,16 +1,21 @@
 class Post
-
-  attr_accessor :title, :author
+  attr_accessor :author, :title
+ 
+  @@all = []
+ 
   def initialize(title)
     @title = title
-    @author = []
+    @@all << self
   end
-
+  
+  def self.all
+    @@all
+  end
+  
   def author_name
-    if @author == nil
-      return nil
-    else
-      @author.name
-    end
+    if self.author
+      self.author.name
+  else
+    nil
   end
 end
