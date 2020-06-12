7d7215da3e91a771484ab890ea958d15ba75f6bd
Elijah Kirschen <ekirschen584@gmail.com>
2020-06-12 01:22:00 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,37 +1,31 @@
-require 'pry'
-
 class Artist
+  
+  attr_accessor :name
 
-  attr_accessor :name, :songs
-
-  @@song_count = 0
-
+@@all =[]
+  
   def initialize(name)
     @name = name
     @songs = []
   end
-
-  def songs 
-  Song.all.select do |song|
-    song.artist == self 
-    end
-  end 
-
+  
   def add_song(song)
-    @songs << song
     song.artist = self
-    @@song_count += 1
+    @songs << song
+  end
+  
+  
+ def songs
+   Song.all.select {|song| song.artist == self}
   end
 
   def add_song_by_name(name)
     song = Song.new(name)
-    @songs << song
-    song.artist = self
-    @@song_count += 1
+    add_song(song)
   end
-
+  
   def self.song_count
-    Song.all.count
+    Song.all.count 
   end
-
+  
 end
